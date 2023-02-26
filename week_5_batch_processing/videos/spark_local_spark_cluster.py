d433247457d80d66c49b372197d6cc0bea2dba2a
Frauke Albrecht <falbrechtg@gmail.com>
2023-02-26 20:16:30 +0000

    week 5 spark local cluster

---
+++ week_5_batch_processing/videos/spark_local_spark_cluster.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--input_green', required=True)
+parser.add_argument('--input_yellow', required=True)
+parser.add_argument('--output', required=True)
+
+args = parser.parse_args()
+
+input_green = args.input_green
+input_yellow = args.input_yellow
+output = args.output
+
+spark = SparkSession.builder \
+    .appName('test') \
+    .getOrCreate()
+
+df_green = spark.read.parquet(input_green)
+df_yellow = spark.read.parquet(input_yellow)
+
+df_green = df_green \
+    .withColumnRenamed('lpep_pickup_datetime', 'pickup_datetime') \
+    .withColumnRenamed('lpep_dropoff_datetime', 'dropoff_datetime')
+
+df_yellow = df_yellow \
+    .withColumnRenamed('tpep_pickup_datetime', 'pickup_datetime') \
+    .withColumnRenamed('tpep_dropoff_datetime', 'dropoff_datetime')
+
+
+common_columns = []
+yellow_columns = set(df_yellow.columns)
+for col in yellow_columns:
+    if col in yellow_columns:
+        common_columns.append(col)
+
+# select common columns and add column "service_type"
+df_green_sel = df_green \
+    .select(common_columns) \
+    .withColumn('service_type', F.lit('green'))
+
+
+df_yellow_sel = df_yellow \
+    .select(common_columns) \
+    .withColumn('service_type', F.lit('yellow'))
+
+
+df_trips_data = df_green_sel.unionAll(df_yellow_sel)
+df_trips_data.groupBy('service_type').count().show()
+
+# register dataframe as a table to use sql
+df_trips_data.registerTempTable('trips_data')
+
+df_result = spark.sql("""
+SELECT 
+    -- Reveneue grouping 
+    PULocationID AS revenue_zone,
+    date_trunc('month', pickup_datetime) AS revenue_month, 
+    service_type, 
+
+    -- Revenue calculation 
+    SUM(fare_amount) AS revenue_monthly_fare,
+    SUM(extra) AS revenue_monthly_extra,
+    SUM(mta_tax) AS revenue_monthly_mta_tax,
+    SUM(tip_amount) AS revenue_monthly_tip_amount,
+    SUM(tolls_amount) AS revenue_monthly_tolls_amount,
+    SUM(improvement_surcharge) AS revenue_monthly_improvement_surcharge,
+    SUM(total_amount) AS revenue_monthly_total_amount,
+    SUM(congestion_surcharge) AS revenue_monthly_congestion_surcharge,
+
+    -- Additional calculations
+    AVG(passenger_count) AS avg_montly_passenger_count,
+    AVG(trip_distance) AS avg_montly_trip_distance
+FROM
+    trips_data
+GROUP BY
+    1, 2, 3
+""")
+
+# write results to parquet and reduce partitions to 1
+df_result.coalesce(1).write.parquet(output, mode='overwrite')
+
